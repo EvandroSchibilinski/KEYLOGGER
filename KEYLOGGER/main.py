0b872cdd5bd3fa4e34d8b2d3620c873a168dc9ce
EvandroSchibilinski <schibi100@icloud.com>
2025-10-06 17:03:16 -0300

    primeiro commit

---
+++ KEYLOGGER/main.py
@@ -0,0 +1,12 @@
+
+""" -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- K E Y L O G G E R -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+ Objetivo deste aplicativo é apenas por aprendizado, não me responsabilizo por uso de maneiras ilícitas
+
+ Esse programa seria um hacker que roda em segundo plano que lê (ouvi) as teclas inseridas pelo Usuario
+"""
+__version__ = "1.0"
+__author__ = "EvandroAbreu"
+__license__ = "Unlicense"
+
+from pynput.keyboard import Listener
+print('teste')
